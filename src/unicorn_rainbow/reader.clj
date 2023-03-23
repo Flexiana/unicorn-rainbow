a00eec655f36bbba082b2bcd0fca77ff6abb725f
Jiri Knesl <jiri@flexiana.com>
2023-03-23 16:59:41 +0000

    delete old code

--- src/unicorn_rainbow/reader.clj
+++ src/unicorn_rainbow/reader.clj
@@ -8,36 +8,6 @@
   (set (map str (with-open [rdr (io/reader filename)]
                   (doall (line-seq rdr))))))
 
-;; (defn remove-emojis [form emojis]
-;;   (let [emoji-pattern (re-pattern (str "[" (apply str emojis) "]"))]
-;;     (clojure.walk/postwalk
-;;      (fn [x]
-;;        (if (and (symbol? x) (re-find emoji-pattern (str x)))
-;;          (symbol (clojure.string/replace (str x) emoji-pattern ""))
-;;          x))
-;;      form)))
-
-(defn remove-emojis [form emojis]
-  (let [emoji-pattern (re-pattern (str "[" (apply str emojis) "]"))]
-    (clojure.walk/postwalk
-     (fn [x]
-       (if (and (symbol? x) (re-find emoji-pattern (clojure.string/trim (str x))))
-         (let [replaced-string (clojure.string/replace (clojure.string/trim (str x)) emoji-pattern "")]
-           (println x replaced-string (empty? replaced-string))
-           (if (empty? replaced-string) nil replaced-string))
-         x))
-     form)
-    ))
-
-(defmacro 🦄🌈 [body]
-  (let [emojis (load-emojis "emojis.txt")
-        cleaned-body (remove-emojis body emojis)]
-    (println (pr-str cleaned-body))
-    (eval cleaned-body)))
-
-
-;; (🦄🌈 (+ 1 1 (comment 🌈)))
-
 (defn identify-emoji [emoji-pattern x]
   (and (symbol? x) (boolean (re-find emoji-pattern (clojure.string/trim (str x))))))
 
