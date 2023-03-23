882c244034950328d2cd5e2c45e81935c138dca1
Jiri Knesl <jiri@flexiana.com>
2023-03-23 17:24:04 +0000

    Stabilised removing of chars, now retaining WIP on retaining type of parent collection

--- src/unicorn_rainbow/reader.clj
+++ src/unicorn_rainbow/reader.clj
@@ -8,33 +8,34 @@
   (set (map str (with-open [rdr (io/reader filename)]
                   (doall (line-seq rdr))))))
 
+(defn create-emoji-pattern [emojis] (re-pattern (str "[" (apply str emojis) "]")))
+
 (defn identify-emoji [emoji-pattern x]
   (and (symbol? x) (boolean (re-find emoji-pattern (clojure.string/trim (str x))))))
 
-(defn remove-emojis-from-collection [coll emoji-pattern]
-  (cond
-    (map? coll) (into {} (remove (comp identify-emoji emoji-pattern second) coll))
-    (set? coll) (into #{} (remove (partial identify-emoji emoji-pattern) coll))
-    (vector? coll) (into [] (remove (partial identify-emoji emoji-pattern) coll))
-    (list? coll) (into '() (remove (partial identify-emoji emoji-pattern) coll))
-    :else (throw (IllegalArgumentException. "Unsupported collection type"))))
+(defn remove-emojis-from-collection [emoji-pattern coll]
 
-(defn remove-emojis2 [body emojis]
   (let [
-        emoji-pattern (re-pattern (str "[" (apply str emojis) "]"))
-        
-        replace-emojis (fn [x]
-                         (println x (seqable? x))
+        removed-in-coll (remove (partial identify-emoji emoji-pattern) coll)
+  ]
+    (cond
+      (map? coll) (into {} removed-in-coll)
+      (set? coll) (into #{} removed-in-coll)
+      (vector? coll) (into [] removed-in-coll)
+      (list? coll) (into '() coll)
+      :else (throw (IllegalArgumentException, "This collection type is not supported"))
+      )))
+
+(defn replace-emojis [emoji-pattern x]
                          (if (seqable? x)
-                           (remove-emojis-from-collection x emoji-pattern)
-                           x))]
-    (clojure.walk/postwalk replace-emojis body)
-    ))
+                           (remove-emojis-from-collection emoji-pattern x)
+                           x))
 
 (defmacro ur [body]
   (let [emojis (load-emojis "emojis.txt")
-        cleaned-body (remove-emojis2 body emojis)]
-    (println (pr-str cleaned-body))
+        emoji-pattern (create-emoji-pattern emojis)
+        cleaned-body (clojure.walk/postwalk (partial replace-emojis emoji-pattern) body)]
+    (println cleaned-body)
     (eval cleaned-body)))
 
-(ur (+ 1 1 #_1 #_🌈 🌈))
+(ur (+ 1 1 🌈 (+ 2 3 🌈)))
