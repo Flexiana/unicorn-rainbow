172b91d127450e761f207a56742b7162beab56af
Jiri Knesl <jiri@flexiana.com>
2023-03-23 17:39:06 +0000

    Now, good luck to anyone who will try to understand this

--- src/unicorn_rainbow/reader.clj
+++ src/unicorn_rainbow/reader.clj
@@ -3,33 +3,28 @@
             [clojure.string]
             [clojure.walk]))
 
-(defn create-emoji-pattern [emojis] (re-pattern (str "[" (apply str emojis) "]")))
-
 (defn identify-emoji [emoji-pattern x]
   (and (symbol? x) (boolean (re-find emoji-pattern (clojure.string/trim (str x))))))
 
-(defn remove-emojis-from-collection [emoji-pattern coll]
-
-  (let [
-        removed-in-coll (remove (partial identify-emoji emoji-pattern) coll)
-  ]
-    (cond
-      (map? coll) (into {} removed-in-coll)
-      (set? coll) (into #{} removed-in-coll)
-      (vector? coll) (into [] removed-in-coll)
-      :else removed-in-coll
-      )))
-
-(defn replace-emojis [emoji-pattern x]
-                         (if (seqable? x)
-                           (remove-emojis-from-collection emoji-pattern x)
-                           x))
 
 (defmacro 🦄🌈 [body]
-  (let [
-        cleaned-body (clojure.walk/postwalk (partial replace-emojis (re-pattern (str "[" (apply str (slurp "emojis.txt")) "]"))) body)]
-    (eval cleaned-body)))
-
+  (eval 
+   (clojure.walk/postwalk 
+    (partial 
+     (fn [emoji-pattern x]
+       (if (seqable? x)
+         ((fn [emoji-pattern coll]
+            (let [removed-in-coll (remove (partial identify-emoji emoji-pattern) coll)]
+              (cond
+                (map? coll) (into {} removed-in-coll)
+                (set? coll) (into #{} removed-in-coll)
+                (vector? coll) (into [] removed-in-coll)
+                :else removed-in-coll))) 
+          emoji-pattern
+          x)
+         x)) 
+     (re-pattern (str "[" (apply str (slurp "emojis.txt")) "]"))) 
+    body)))
 
 (println (🦄🌈 (+ 1 1 🌈 (+ 2 3 🌈))))
 (println (🦄🌈 (fn [🌈🌈 🌈] (+ 1 1 🌈 (+ 2 3 🌈)))))
