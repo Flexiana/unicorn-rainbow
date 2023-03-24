8f4d73bdfa04c0f2daa9ccab5361607545c77a49
Jiri Knesl <jiri@flexiana.com>
2023-03-23 20:48:03 -0400

    Update project.clj
    Clean dependencies

--- project.clj
+++ project.clj
@@ -3,7 +3,5 @@
   :url "http://github.com/flexiana/unicorn-rainbow"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]
-                 [org.clojure/tools.reader "1.3.6"]
-                 [com.vdurmont/emoji-java "5.1.1"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]]
   :repl-options {:init-ns unicorn-rainbow.core})
